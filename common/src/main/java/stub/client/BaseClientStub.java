880ea83bdc2a7d74186490ff6baf072b176cb764
StrokeBun <stroke0.0bun@gmail.com>
2021-05-12 16:45:35 +0800

    fix the inheritance of stub class

--- common/src/main/java/stub/client/BaseClientStub.java
+++ common/src/main/java/stub/client/BaseClientStub.java
@@ -1,38 +1,37 @@
-package stub.socket;
+package stub.client;
 
 import config.RpcClientConfiguration;
 import dto.Request;
 import dto.Response;
-import exception.enums.RpcErrorMessageEnum;
 import exception.RpcException;
+import exception.enums.RpcErrorMessageEnum;
 import registry.ServiceDiscovery;
-import serialize.serializer.iostream.IOStreamSerializer;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.util.UUID;
 
 /**
- * @description: the stub of client
+ * @description: base implement of client stub
  * @author: Stroke
- * @date: 2021/04/21
+ * @date: 2021/05/12
  */
-public class SocketClientStub {
+public abstract class BaseClientStub implements ClientStub {
     private static final String INTERFACE_NAME = "interfaceName";
     private RpcClientConfiguration configuration;
 
-    public SocketClientStub() {
+    public BaseClientStub() {
         configuration = RpcClientConfiguration.builder().build();
     }
 
-    public SocketClientStub(RpcClientConfiguration configuration) {
+    public BaseClientStub(RpcClientConfiguration configuration) {
         this.configuration = configuration;
     }
 
+    @Override
     public<T> T getInstance(Class<T> clazz) {
         class ClientHandler implements InvocationHandler {
             @Override
@@ -49,31 +48,28 @@ public class SocketClientStub {
                 // discover the rpc server socket address
                 final ServiceDiscovery discovery = configuration.getServiceDiscovery();
                 InetSocketAddress address = discovery.discoverService(className);
-                try (Socket socket = new Socket(address.getAddress(), address.getPort())) {
-                    // generate request and send it to rpc server
-                    Request request = new Request();
-                    request.setRequestId(UUID.randomUUID().toString());
-                    request.setClassName(className);
-                    request.setMethodName(method.getName());
-                    request.setParametersType(method.getParameterTypes());
-                    request.setParametersValue(args);
-                    final IOStreamSerializer serializer = configuration.getSerializer();
-                    serializer.serialize(request, socket.getOutputStream());
 
-                    // get response from rpc server
-                    Response response = serializer.deserialize(socket.getInputStream(), Response.class);
-                    // check the response
-                    check(request, response);
-                    return response.getResult();
-                }
+                // generate request and send it to rpc server
+                Request request = new Request();
+                request.setRequestId(UUID.randomUUID().toString());
+                request.setClassName(className);
+                request.setMethodName(method.getName());
+                request.setParametersType(method.getParameterTypes());
+                request.setParametersValue(args);
+
+                Response response = sendRequest(request, address, configuration);
+                // check the response
+                check(request, response);
+                return response.getResult();
             }
         }
         Object object = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new ClientHandler());
-        return (T)object;
+        return clazz.cast(object);
     }
 
+    public abstract Response sendRequest(Request request, InetSocketAddress address, RpcClientConfiguration configuration) throws IOException;
 
-    public void check(Request request, Response response) {
+    private void check(Request request, Response response) {
         if (response == null) {
             throw new RpcException(RpcErrorMessageEnum.SERVICE_INVOCATION_FAILURE, INTERFACE_NAME + ":" + request.getMethodName());
         }
